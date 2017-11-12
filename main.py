b4b2cd0fd449d373b532b54328042c907fcb77da
Alicia <aliciakramme@gmail.com>
2017-11-12 15:03:36 -0600

    completed form and added caesar function

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web Caesar App</title>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method= "post">
+            <label for="rot">Rotate by:</label>
+                <input id="rot" type="text" name="rot" value="0"/>
+            <label for="textarea">Type your message:</label>
+                <input id="textarea" type="textarea" name="text"/>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
+</html>
+"""
+
+
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = request.form['text']
+
+    encryption = rotate_string(text, rot)
+    return '<h1>' + encryption + '</h1>'
+
+
+app.run()
